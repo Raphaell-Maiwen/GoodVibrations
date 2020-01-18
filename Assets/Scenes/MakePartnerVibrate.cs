e8caa3b2813c932f188aa963f00cdbbfa54f093e
Raphaëll <raphaelle.mp@gmail.com>
2020-01-18 00:30:30 -0500

    The game

---
+++ Assets/Scenes/MakePartnerVibrate.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+using System.Linq;
+
+public class MakePartnerVibrate : MonoBehaviour
+{
+    //PlayerIndex playerIndex;
+    Partner partner0;
+    Partner partner1;
+
+    GamePadState state0;
+    GamePadState state1;
+
+    //string[] buttons;
+
+    float vibratingLength = 1f;
+    float vibratingTime = 0f;
+    bool something = true;
+
+    float vibratingLength1 = 1f;
+    float vibratingTime1 = 0f;
+    bool something1 = true;
+
+    enum button
+    {
+        A,
+        B,
+        X,
+        Y
+    }
+
+    // Start is called before the first frame update
+    void Awake() {
+        //buttons = new string[]{"UpButton", "DownButton", "LeftButton", "RightButton"};
+
+        partner0 = new Partner((PlayerIndex)0, RandomButton());
+        partner1 = new Partner((PlayerIndex)1, RandomButton());
+
+        //For testing purposes
+
+        //partner0 = new Partner(0, buttons[Random.Range(0,4)]);
+
+        state0 = GamePad.GetState(((PlayerIndex)0));
+        state1 = GamePad.GetState((PlayerIndex)1);
+    }
+
+    void FixedUpdate() {
+        //old
+        /*GamePad.SetVibration((PlayerIndex)0, state1.Triggers.Left, state1.Triggers.Right);
+        GamePad.SetVibration((PlayerIndex)1, state0.Triggers.Left, state0.Triggers.Right);*/
+
+        //GamePad.SEt
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //old
+        /*state0 = GamePad.GetState((PlayerIndex)0);
+        state1 = GamePad.GetState((PlayerIndex)1);*/
+
+        partner0.state = GamePad.GetState(partner0.playerIndex);
+        partner1.state = GamePad.GetState(partner1.playerIndex);
+
+        if (partner0.state.Buttons.A == ButtonState.Pressed && partner0.currentButton == button.A && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner1.playerIndex, 0.5f, 0.5f);
+            something = false;
+        }
+        else if (partner0.state.Buttons.B == ButtonState.Pressed && partner0.currentButton == button.B && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner1.playerIndex, 0.5f, 0.5f);
+            something = false;
+        }
+        else if (partner0.state.Buttons.X == ButtonState.Pressed && partner0.currentButton == button.X && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner1.playerIndex, 0.5f, 0.5f);
+            something = false;
+        }
+        else if (partner0.state.Buttons.Y == ButtonState.Pressed && partner0.currentButton == button.Y && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner1.playerIndex, 0.5f, 0.5f);
+            something = false;
+        }
+
+
+
+
+        if (partner1.state.Buttons.A == ButtonState.Pressed && partner1.currentButton == button.A && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner0.playerIndex, 0.5f, 0.5f);
+            something1 = false;
+        }
+        else if (partner1.state.Buttons.B == ButtonState.Pressed && partner1.currentButton == button.B && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner0.playerIndex, 0.5f, 0.5f);
+            something1 = false;
+        }
+        else if (partner1.state.Buttons.X == ButtonState.Pressed && partner1.currentButton == button.X && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner0.playerIndex, 0.5f, 0.5f);
+            something1 = false;
+        }
+        else if (partner1.state.Buttons.Y == ButtonState.Pressed && partner1.currentButton == button.Y && something) {
+            Debug.Log("Yo");
+            GamePad.SetVibration(partner0.playerIndex, 0.5f, 0.5f);
+            something1 = false;
+        }
+
+        //+Add some gauge
+        if (!something) {
+            vibratingTime += Time.deltaTime;
+            if (vibratingTime >= vibratingLength) {
+                something = true;
+                GamePad.SetVibration(partner1.playerIndex, 0f, 0f);
+                vibratingTime = 0f;
+            }
+        }
+
+
+        if (!something1) {
+            vibratingTime1 += Time.deltaTime;
+            if (vibratingTime1 >= vibratingLength1) {
+                something1 = true;
+                GamePad.SetVibration(partner0.playerIndex, 0f, 0f);
+                vibratingTime1 = 0f;
+            }
+        }
+    }
+
+    button RandomButton() {
+        System.Array values = System.Enum.GetValues(typeof(button));
+        System.Random r = new System.Random();
+        button randomButton = (button)values.GetValue(r.Next(values.Length));
+
+        return randomButton;
+    }
+
+    class Partner
+    {
+        public GamePadState state;
+        public PlayerIndex playerIndex;
+
+        public button currentButton;
+        public bool leftVibrate = false;
+        public bool rightVibrate = false;
+
+        public Partner(PlayerIndex newIndex, button newCurrentButton) {
+            playerIndex = newIndex;
+            currentButton = newCurrentButton;
+        }
+    }
+}
---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+/*
+Adding a gauge so you can press slightly before end of vibration
+Message of getting too slow or too fast
+Intensity increases after several good hits (in a row)
+Intensity decreases after several (not in a row, total of the current stage)
+Max intensity = automatically vibrates
+After 2 or 3 intensity decrease, it changes buttons
+
+Be more gentle at first?
+
+Add joystick interactions:
+-Circular clockwise
+-Circular anticlockwise
+-Left and right
+-Up and down
+*/
+public class Player : MonoBehaviour
+{
+    public int playerInd;
+    PlayerIndex otherPlayer;
+
+    GamePadState state;
+    string[] buttons;
+    string currentButton;
+
+    //Minimum base vibration: 0.2f
+    //Maximum base vibration: 2f
+    //First range: 0.2 - 0.85
+    //Second range: 0.86 - 1.35
+    //Third range: 1.36 - 2.00
+    float buttonVibratingLength;
+    float vibratingTime;
+    bool isVibrating;
+
+    float pressButtonTimeWindow;
+
+    float lastTime;
+
+    void Awake()
+    {
+        if (playerInd == 1) {
+            otherPlayer = (PlayerIndex)1;
+        }
+        else {
+            otherPlayer = (PlayerIndex)0;
+        }
+
+        buttons = new string[4];
+        buttons[0] = "BottomButtonP" + playerInd;
+        buttons[1] = "UpButtonP" + playerInd;
+        buttons[2] = "LeftButtonP" + playerInd;
+        buttons[3] = "RightButtonP" + playerInd ;
+
+        currentButton = buttons[Random.Range(0, buttons.Length)];
+
+        Debug.Log(currentButton);
+
+        buttonVibratingLength = 0.2f;
+        pressButtonTimeWindow = 0.05f;
+
+        //SetupVibratingLength();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //Debug.Log("H: " + Input.GetAxis("HorizontalP" + 1) + " V: " + Input.GetAxis("VerticalP1"));
+
+        //Turn off the vibration after the predetermined length
+        if (isVibrating) {
+            vibratingTime += Time.deltaTime;
+            if (vibratingTime >= buttonVibratingLength) {
+                vibratingTime = 0;
+                isVibrating = false;
+                GamePad.SetVibration(otherPlayer, 0f, 0f);
+            }
+        }
+
+        //Turn on the vibration if the predetermined button is pushed
+        if (Input.GetButtonDown(currentButton) && !isVibrating) {
+            print(currentButton + " " + playerInd);
+            GamePad.SetVibration(otherPlayer, 0.5f, 0.5f);
+            isVibrating = true;
+            lastTime = Time.time;
+        }
+
+        
+    }
+
+    void SetupVibratingLength() {
+        int speedLevel = Random.Range(0, 100);
+
+        if (speedLevel < 20) {
+            buttonVibratingLength = Random.Range(0.2f, 0.85f);
+        }
+        else if (speedLevel < 80) {
+            buttonVibratingLength = Random.Range(0.86f, 1.35f);
+        }
+        else {
+            buttonVibratingLength = Random.Range(1.36f, 2f);
+        }
+    }
+}
